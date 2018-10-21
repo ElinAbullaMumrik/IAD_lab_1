aee158d7099c0192d98f7e2fad90e13e44dec5f7
Люда <lyuda_shm@mail.ru>
2018-10-21 22:12:33 +0300

    Initial commit

---
+++ check.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Людмила
+ * Date: 07.09.2018
+ * Time: 21:25
+ */
+session_start();
+$currentTime = date("H:i:s", strtotime('-1 hour'));
+$start = microtime(true);
+$x = (float)str_replace(",", ".", $_POST['X']);
+$y = (int)$_POST['Y'];
+$r = (int)$_POST['R'];
+function check($x, $y, $r)
+{
+    return ($x <= 0 && $x >= -$r / 2 && $y <= 0 && $y >= -$r) ||
+        ($x >= 0 && $y >= 0 && $x * $x + $y * $y <= $r * $r) ||
+        ($x >= 0 && $y <= 0 && $x <= -$r / 2 + $y);
+}
+
+if (!in_array($y, array(-4, -3, -2, -1, 0, 1, 2, 3, 4)) || !is_numeric($x) || $x < -3 || $x > 3 || !in_array($r, array(1, 1.5, 2, 2.5, 3))) {
+    http_response_code(400);
+    return;
+}
+$res = check($x, $y, $r);
+$time = microtime(true) - $start;
+$resultat = array($x, $y, $r, $res, $currentTime, $time);
+if (!isset($_SESSION['table'])) {
+    $_SESSION['table'] = array();
+}
+
+array_push($_SESSION['table'], $resultat);
+include "table.php";
